5717aa8ae78a7a8712f33f52abb7e5322cd6f3ce
Jesus Eduardp <eduardoCastillo180@hotmail.com>
2022-08-05 17:43:55 -0400

    Se cumple con objetivo 01Button, se logra git push origin master

---
+++ testParts/01buttonAnimateAuto.js
@@ -0,0 +1,99 @@
+/*Definir una clase boton al cual se le pueda ser asiganado una animacion, utilizando la API web*/
+/*OBJETIVO:
+animar button*/ 
+
+
+class button 
+{
+    constructor(objectAnimation)// se le pasara por parametro la un objetoAnimation
+    {
+        this.objectAnimation=objectAnimation;
+    }
+
+    animationTo= function(buttonRecived)
+    {   
+        let click= false;
+        let begginx= this.objectAnimation.beggin;
+        let optionx= this.objectAnimation.optionFrames;
+        let endingx=this.objectAnimation.ending;
+        
+        function animate(){
+     
+        console.log(begginx)
+       
+
+        switch(click){
+
+        
+            case false:
+                buttonRecived.animate(begginx, optionx);
+                click=true;
+            break;
+            
+            case true:
+            
+                buttonRecived.animate(endingx, optionx);
+                click=false;
+        }
+        
+
+        }
+
+        return animate
+    }
+}
+
+
+
+/*Ahora bien el abjectAnimation seria:*/ 
+let objectAnimation=
+{
+    beggin: [{transform: "rotate(360deg)"}],//fotogramas claves
+    ending: [{transform: "rotate(-180deg)"}],//fotogramas claves; animacion de cierre
+    optionFrames: { duration: 1000, iterations:1}
+}
+
+
+
+/*Instanciar la clase*/
+let test1= new button(objectAnimation)
+
+ let elementFromDom= document.querySelector(".btn");
+let btnOne=test1.animationTo(elementFromDom);
+let btnTest= document.querySelector(".btnTest");
+
+btnTest.addEventListener("click", btnOne);
+
+
+
+let test2= new button(objectAnimation);
+
+let elementFromDomKamila= document.querySelector(".btnKamila")
+let btnTwo=test2.animationTo(elementFromDomKamila);
+let btnTestKamila= document.querySelector(".btnTestKamila");
+
+btnTestKamila.addEventListener("click", btnTwo);
+
+
+
+
+
+
+
+
+/* Ejemplo de Clousure
+function fun(user)
+{
+    let nombre=user;
+
+    function clo()
+    {
+         console.log(nombre);
+    }
+    return clo
+}
+
+let test=fun();
+let test2=fun("jesus");
+test()
+test2()*/
